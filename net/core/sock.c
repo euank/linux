e6f13b7a2439189de2c083f4dd23dd292ab1febf
Anna Emese Nyiri <annaemesenyiri@gmail.com>
2024-12-13 09:44:57 +0100

    sock: Introduce SO_RCVPRIORITY socket option
    Add new socket option, SO_RCVPRIORITY, to include SO_PRIORITY in the
    ancillary data returned by recvmsg().
    This is analogous to the existing support for SO_RCVMARK,
    as implemented in commit 6fd1d51cfa253 ("net: SO_RCVMARK socket option
    for SO_MARK with recvmsg()").
    
    Reviewed-by: Willem de Bruijn <willemb@google.com>
    Suggested-by: Ferenc Fejes <fejes@inf.elte.hu>
    Signed-off-by: Anna Emese Nyiri <annaemesenyiri@gmail.com>
    Link: https://patch.msgid.link/20241213084457.45120-5-annaemesenyiri@gmail.com
    Signed-off-by: Jakub Kicinski <kuba@kernel.org>

--- net/core/sock.c
+++ net/core/sock.c
@@ -1522,6 +1522,10 @@ set_sndbuf:
 		sock_valbool_flag(sk, SOCK_RCVMARK, valbool);
 		break;
 
+	case SO_RCVPRIORITY:
+		sock_valbool_flag(sk, SOCK_RCVPRIORITY, valbool);
+		break;
+
 	case SO_RXQ_OVFL:
 		sock_valbool_flag(sk, SOCK_RXQ_OVFL, valbool);
 		break;
@@ -1948,6 +1952,10 @@ int sk_getsockopt(struct sock *sk, int level, int optname,
 
 	case SO_RCVMARK:
 		v.val = sock_flag(sk, SOCK_RCVMARK);
+		break;
+
+	case SO_RCVPRIORITY:
+		v.val = sock_flag(sk, SOCK_RCVPRIORITY);
 		break;
 
 	case SO_RXQ_OVFL:
