dd1c825201ddb4bf827310d2239fd89148361ffa
Wen Gong <quic_wgong@quicinc.com>
2025-03-27 10:05:25 +0800

    wifi: ath12k: use correct WMI command to set country code for WCN7850
    When userspace tries to set country code by NL80211_REGDOM_SET_BY_USER
    hint(like iw reg set XX), it will pass new country code to ath12k.
    Then ath12k will set this new country code to firmware by
    WMI_SET_INIT_COUNTRY_CMDID.
    
    For AP based chips(QCN92xx), WMI_SET_INIT_COUNTRY_CMDID is the correct
    command. However, for STATION based chips(WCN7850), it need to use
    WMI_SET_CURRENT_COUNTRY_CMDID.
    
    Add flag current_cc_support in hardware parameters. It is used to
    distinguish AP/STA platform. After that, the firmware will work
    normally and the regulatory feature works well for WCN7850.
    
    Tested-on: WCN7850 hw2.0 PCI WLAN.HMT.1.0.c5-00481-QCAHMTSWPL_V1.0_V2.0_SILICONZ-3
    
    Signed-off-by: Wen Gong <quic_wgong@quicinc.com>
    Signed-off-by: Kang Yang <quic_kangyang@quicinc.com>
    Reviewed-by: Vasanthakumar Thiagarajan <vasanthakumar.thiagarajan@oss.qualcomm.com>

--- drivers/net/wireless/ath/ath12k/hw.c
+++ drivers/net/wireless/ath/ath12k/hw.c
@@ -932,6 +932,7 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 		.iova_mask = 0,
 
 		.supports_aspm = false,
+		.current_cc_support = false,
 	},
 	{
 		.name = "wcn7850 hw2.0",
@@ -1012,6 +1013,8 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 		.iova_mask = ATH12K_PCIE_MAX_PAYLOAD_SIZE - 1,
 
 		.supports_aspm = true,
+
+		.current_cc_support = true,
 	},
 	{
 		.name = "qcn9274 hw2.0",
@@ -1088,6 +1091,8 @@ static const struct ath12k_hw_params ath12k_hw_params[] = {
 		.iova_mask = 0,
 
 		.supports_aspm = false,
+
+		.current_cc_support = false,
 	},
 };
 
